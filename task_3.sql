318af577d515c66f456c32381c753c57a295ec02
Sophy <snasambu40@gmail.com>
2025-08-17 12:40:32 +0300

    Add USE statement for alx_book_store in task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,2 @@
-﻿-- task_3.sql
--- This script lists all tables in a database passed as an argument
-
-SHOW TABLES;
+﻿USE alx_book_store;
+-- task_3.sql -- This script lists all tables in a database passed as an argument  SHOW TABLES;
